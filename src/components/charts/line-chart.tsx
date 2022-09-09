188daeaa15f61640efa4b4aa3eff71f2317a5715
yoyobojo <angellolazar@gmail.com>
2022-09-09 15:14:31 -0400

    added time series

--- src/components/charts/line-chart.tsx
+++ src/components/charts/line-chart.tsx
@@ -1,10 +1,14 @@
-import { lineData } from '../../utils/mock-data';
 import React, { MouseEvent } from 'react';
 import { LineChart, Line, Tooltip, ResponsiveContainer } from 'recharts';
 import { CustomTooltip } from './custom-tooltip';
 
+export type IDataProps = {
+  date: string;
+  value: number;
+}
+
 type ILineChartProps = {
-  data?: Array<any>;
+  data: Array<IDataProps>;
   dataKey?: string;
   loading?: boolean;
   error?: boolean;
@@ -13,6 +17,7 @@ type ILineChartProps = {
 }
 
 const ReLineChart = (props: ILineChartProps) => {
+  const [datedData, setDatedData] = React.useState(props.data);
   const [active, setActive] = React.useState("all");
 
   const determineActive = (str: string) => {
@@ -20,14 +25,41 @@ const ReLineChart = (props: ILineChartProps) => {
   }
 
   const handleClick = (e:any) => {
-    const eventText = e.target.innerHTML;
-    setActive(eventText.toLowerCase());
+    const eventText = e.target.innerHTML.toLowerCase();
+    setActive(eventText);
+
+    const toDate = new Date();
+    let fromDate: Date;
+    switch(eventText) {
+      case '1d':
+        fromDate = new Date(toDate.setDate(toDate.getDate() - 1));
+        break;
+      case '1w':
+        fromDate = new Date(toDate.setDate(toDate.getDate() - 7));
+        break;
+      case '1m':
+        fromDate = new Date(toDate.setDate(toDate.getDate() - 30));
+        break;
+      case '1y':
+        fromDate = new Date(toDate.setDate(toDate.getDate() - 365));
+        break;
+      default:
+        setDatedData(props.data);
+        return;
+    }
+
+    const rangeData = props.data.filter((el: any) => {
+      return new Date(el.date).getTime() >= fromDate.getTime() &&
+             new Date(el.date).getTime() <= new Date().getTime();
+    });
+
+    setDatedData(rangeData);
   }
 
   return (
     <>
       {props.timeseries && (
-        <div className={`flex gap-3 text-neutral-500`}>
+        <div className={`flex gap-2 text-neutral-500 text-sm`}>
           <button className={`p-1 ${determineActive("1d")}`} onClick={handleClick}>1D</button>
           <button className={`p-1 ${determineActive("1w")}`} onClick={handleClick}>1W</button>
           <button className={`p-1 ${determineActive("1m")}`} onClick={handleClick}>1M</button>
@@ -39,7 +71,7 @@ const ReLineChart = (props: ILineChartProps) => {
         <LineChart
           width={500}
           height={300}
-          data={props.data || lineData}
+          data={datedData}
           margin={{
             top: 10,
             bottom: 10,
