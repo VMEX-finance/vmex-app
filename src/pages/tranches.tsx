7d4cf0de506b889583947fed8a03e6533b5dd05c
aedsidhe <hughesy102@gmail.com>
2022-10-28 16:50:21 -0400

    begin new table template

--- src/pages/tranches.tsx
+++ src/pages/tranches.tsx
@@ -4,7 +4,7 @@ import { AppTemplate } from '../ui/templates';
 import { TranchesTable } from '../ui/components/tables';
 import { DropdownButton } from '../ui/components/buttons';
 import { _mockTranchesData } from '../utils/mock-data';
-
+import { TranchesTableDos } from '../ui/components/tables/tranches2';
 const Tranches: React.FC = () => {
     return (
         <AppTemplate title="tranches">
@@ -12,10 +12,27 @@ const Tranches: React.FC = () => {
                 <p className="text-xl pb-5">All Available Tranches</p>
                 <div className="divide-x-8 divide-transparent">
                     <DropdownButton items={[{ text: 'Filter Asset' }]} primary direction="right" />
-                    <DropdownButton items={[{ text: 'Filter APY' }]} primary direction="right" />
-                    <DropdownButton items={[{ text: 'Filter TVL' }]} primary direction="right" />
+                    <DropdownButton
+                        items={[
+                            { text: 'Filter Rating' },
+                            { text: 'High to Low' },
+                            { text: 'Low to High' },
+                        ]}
+                        primary
+                        direction="right"
+                    />
+                    <DropdownButton
+                        items={[
+                            { text: 'Filter TVL' },
+                            { text: 'High to Low' },
+                            { text: 'Low to High' },
+                        ]}
+                        primary
+                        direction="right"
+                    />
                 </div>
-                <TranchesTable data={_mockTranchesData} />
+                {/*<TranchesTable data={_mockTranchesData} /> */}
+                <TranchesTableDos data={_mockTranchesData} />
             </Card>
         </AppTemplate>
     );
