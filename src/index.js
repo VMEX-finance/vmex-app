ac8e1c0ffc8687e3c0f669a0a801cc8ba3d8afcd
aedsidhe <hughesy102@gmail.com>
2022-11-23 14:46:53 -0600

    initial work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ReduxProvider from './store/redux';
+import { wagmiClient, chains, WagmiConfig, RainbowKitProvider } from './store/rainbow';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
