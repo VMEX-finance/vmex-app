e0338acd81bc8b0d4b8ec43cc91e3a0a56ba375c
yoyobojo <angellolazar@gmail.com>
2022-12-13 15:01:04 -0500

    started on specific tranche subgraph data

--- src/api/subgraph/getChartData.ts
+++ src/api/subgraph/getChartData.ts
@@ -35,13 +35,34 @@ const getTVLChartData = async () => {
     console.log('getTVLChartData:', data);
 };
 
-export function useSubgraphChartData() {
+const getTrancheChartData = async (trancheId?: string | number) => {
+    if (!trancheId) return [];
+    const { data, error } = await client.query({
+        query: gql`
+            query QueryTranche {
+                tranche(id: trancheId) {
+                    id
+                }
+            }
+        `,
+    });
+
+    console.log('getTrancheChartData:', data);
+};
+
+export function useSubgraphChartData(trancheId?: string | number) {
     const queryTVLChartData = useQuery({
         queryKey: ['subgraph-charts-tvl'],
         queryFn: () => getTVLChartData(),
     });
 
+    const queryTrancheChartData = useQuery({
+        queryKey: ['subgraph-charts-asset'],
+        queryFn: () => getTrancheChartData(trancheId),
+    });
+
     return {
         queryTVLChartData,
+        queryTrancheChartData,
     };
 }
