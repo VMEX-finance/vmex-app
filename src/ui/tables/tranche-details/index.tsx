ac40e8d671fc99aac6b8f9fda72ab8d170c85f88
yoyobojo <angellolazar@gmail.com>
2023-01-17 12:36:34 -0500

    fixed check sizing

--- src/ui/tables/tranche-details/index.tsx
+++ src/ui/tables/tranche-details/index.tsx
@@ -155,11 +155,11 @@ export const TrancheTable: React.FC<ITableProps> = ({ data, type }) => {
                                 <td>{el.apy}</td>
                                 <td>
                                     {type === 'supply' ? (
-                                        <div className="w-10 h-10">
+                                        <div className="w-8 h-8">
                                             {el.canBeCollat ? (
-                                                <BsCheck className="w-full h-full text-emerald-600" />
+                                                <BsCheck className="w-full h-full text-green-500" />
                                             ) : (
-                                                <IoIosClose className="w-full h-full text-red-600" />
+                                                <IoIosClose className="w-full h-full text-red-500" />
                                             )}
                                         </div>
                                     ) : (
