1a6412c4900f94eac690881eb82428a17ab7e39c
yoyobojo <angellolazar@gmail.com>
2023-02-06 11:47:54 -0500

    fixed connect wallet width and disabled modals when chain is unsupported

--- src/pages/tranches.tsx
+++ src/pages/tranches.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { AppTemplate } from '../ui/templates';
 import { TranchesTable } from '../ui/tables';
-import { useAccount, useNetwork } from 'wagmi';
+import { useAccount } from 'wagmi';
 import { useSubgraphTranchesOverviewData, useUserData } from '../api';
 import { ITrancheProps } from '@app/api/types';
-import { WalletButton } from '../ui/components';
 
 const Tranches: React.FC = () => {
     const { address } = useAccount();
-    const { chain } = useNetwork();
     const { queryAllTranches } = useSubgraphTranchesOverviewData();
     const { queryUserActivity } = useUserData(address);
 
@@ -21,16 +19,6 @@ const Tranches: React.FC = () => {
                 loading={queryAllTranches.isLoading}
                 userActivity={queryUserActivity}
             />
-            {(chain?.unsupported as any) && (
-                <div className="mt-10 text-center flex-col">
-                    <div className="mb-4">
-                        <span className="text-lg dark:text-neutral-200">
-                            Please switch networks.
-                        </span>
-                    </div>
-                    <WalletButton primary className="w-fit" />
-                </div>
-            )}
         </AppTemplate>
     );
 };
