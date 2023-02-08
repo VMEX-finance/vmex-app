ebab58142c266832a5135be5d96843174722faf6
yoyobojo <angellolazar@gmail.com>
2023-02-08 11:41:54 -0500

    accordions working - styling needed

---
+++ src/ui/components/toggles/accordion.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import { Typography } from '@mui/material';
+
+type IAccordionProps = {
+    title: string;
+    summary: React.ReactNode;
+    details: React.ReactNode;
+};
+
+const Accordion = styled((props: AccordionProps) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    '&:not(:last-child)': {
+        borderBottom: 0,
+    },
+    '&:before': {
+        display: 'none',
+    },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+    <MuiAccordionSummary
+        expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+        {...props}
+    />
+))(({ theme }) => ({
+    backgroundColor:
+        theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(0, 0, 0, .03)',
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+        transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+        marginLeft: theme.spacing(1),
+    },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(0),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+export function DefaultAccordion({ title, summary, details }: IAccordionProps) {
+    const [expanded, setExpanded] = React.useState<string | false>('');
+
+    const handleChange = (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+        setExpanded(newExpanded ? panel : false);
+    };
+
+    return (
+        <Accordion expanded={expanded === title} onChange={handleChange(title)}>
+            <AccordionSummary aria-controls={`${title}-content`} id={`${title}-header`}>
+                {summary}
+            </AccordionSummary>
+            <AccordionDetails>{details}</AccordionDetails>
+        </Accordion>
+    );
+}
--- src/ui/tables/portfolio/your-supplies.tsx
+++ src/ui/tables/portfolio/your-supplies.tsx
@@ -4,20 +4,12 @@ import {
     BasicToggle,
     NumberAndDollar,
     HealthFactor,
-    renderHealth,
+    DefaultAccordion,
 } from '../../components';
 import { BigNumber } from 'ethers';
 import { useWindowSize, useDialogController } from '../../../hooks';
 import { bigNumberToNative, determineHealthColor } from '../../../utils';
-
-import MUIDataTable, { FilterType, Responsive, SelectableRows } from 'mui-datatables';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { Accordion, AccordionDetails, AccordionSummary } from '@material-ui/core';
 
 export type IYourSuppliesTableItemProps = {
     asset: string;
@@ -52,132 +44,86 @@ export const YourSuppliesTable: React.FC<IYourSuppliesTableProps> = ({
         }
     }, [data]);
 
-    //['Asset', 'Amount', 'Collateral', 'APY%', 'Tranche', 'Health'
-    const columns = [
-        {
-            name: 'Tranche Id',
-        },
-        {
-            name: 'Tranche Name',
-        },
-        {
-            name: 'Health',
-            options: {
-                filter: true,
-                customBodyRender: (value: number, tableMeta: any, updateValue: any) => {
-                    return renderHealth(value, 'sm', healthLoading || true);
-                },
-            },
-        },
-    ];
-
-    const options = {
-        filter: true,
-        onFilterChange: (changedColumn: any, filterList: any) => {
-            console.log(changedColumn, filterList);
-        },
-        selectableRows: 'single' as SelectableRows,
-        filterType: 'dropdown' as FilterType,
-        responsive: 'standard' as Responsive,
-        rowsPerPage: 10,
-        expandableRows: true,
-        renderExpandableRow: (rowData: any, rowMeta: any) => {
-            console.log('Tranche id: ', rowData[0]);
-            return (
-                <React.Fragment>
-                    <tr>
-                        <td colSpan={6}>
-                            <TableContainer component={Paper}>
-                                <Table style={{ minWidth: '650' }} aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell align="right">Asset</TableCell>
-                                            <TableCell align="right">Amount</TableCell>
-                                            <TableCell align="right">Collateral</TableCell>
-                                            <TableCell align="right">APY%</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {data
-                                            .filter((el) => el.trancheId == rowData[0])
-                                            .map((i, index) => {
-                                                return (
-                                                    <TableRow
-                                                        key={`${i.trancheId}-${i.asset}`}
-                                                        className="text-left transition duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-900 hover:cursor-pointer"
-                                                        onClick={(e) => {
-                                                            e.preventDefault();
-                                                            openDialog('loan-asset-dialog', {
-                                                                ...i,
-                                                                view: 'Withdraw',
-                                                            });
-                                                        }}
-                                                    >
-                                                        <TableCell align="right">
-                                                            <AssetDisplay
-                                                                name={width > 600 ? i.asset : ''}
-                                                                logo={`/coins/${i.asset?.toLowerCase()}.svg`}
-                                                            />
-                                                        </TableCell>
-                                                        <TableCell align="right">
-                                                            <NumberAndDollar
-                                                                value={`${bigNumberToNative(
-                                                                    i.amountNative,
-                                                                    i.asset,
-                                                                )} ${width > 600 ? i.asset : ''}`}
-                                                                dollar={i.amount}
-                                                                size="xs"
-                                                                color="text-brand-black"
-                                                            />
-                                                        </TableCell>
-                                                        <TableCell align="right">
-                                                            <BasicToggle
-                                                                checked={checked[index]}
-                                                                onClick={(e: any) => {
-                                                                    e.preventDefault();
-                                                                    openDialog(
-                                                                        'toggle-collateral-dialog',
-                                                                        {
-                                                                            ...i,
-                                                                            checked,
-                                                                            setChecked,
-                                                                            index,
-                                                                            trancheId: i.trancheId,
-                                                                        },
-                                                                    );
-                                                                    e.stopPropagation();
-                                                                }}
-                                                                size="small"
-                                                            />
-                                                        </TableCell>
-                                                        <TableCell align="right">
-                                                            {i.apy}%
-                                                        </TableCell>
-                                                    </TableRow>
-                                                );
-                                            })}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </td>
-                    </tr>
-                </React.Fragment>
-            );
-        },
-        page: 1,
-    };
+    const headers = withHealth
+        ? ['Asset', 'Amount', 'Collateral', 'APY%', 'Health']
+        : ['Asset', 'Amount', 'Collateral', 'APY%'];
 
     return (
-        <MUIDataTable
-            title={'Your Supplied Tranches'}
-            data={data
-                .filter(
-                    (elem, index) =>
-                        data.findIndex((obj) => obj.trancheId === elem.trancheId) === index,
-                )
-                .map((el) => [el.trancheId, el.tranche, el.healthFactor])}
-            columns={columns}
-            options={options}
-        />
+        <table className="min-w-full divide-y-2 divide-gray-300 dark:divide-neutral-800 font-basefont">
+            <thead className="">
+                <tr>
+                    {headers.map((el, i) => (
+                        <th
+                            key={`table-header-${i}`}
+                            scope="col"
+                            className={`py-3 text-left text-sm font-semibold text-neutral900 first-of-type:pl-2 first-of-type:md:pl-6`}
+                        >
+                            {el}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 dark:divide-neutral-800">
+                {data.map((i, index) => {
+                    return (
+                        <tr
+                            key={`${i.trancheId}-${i.asset}`}
+                            className="text-left transition duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-900 hover:cursor-pointer"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                openDialog('loan-asset-dialog', {
+                                    ...i,
+                                    view: 'Withdraw',
+                                });
+                            }}
+                        >
+                            <td className="whitespace-nowrap p-4 text-sm sm:pl-6">
+                                <AssetDisplay
+                                    name={width > 600 ? i.asset : ''}
+                                    logo={`/coins/${i.asset?.toLowerCase()}.svg`}
+                                />
+                            </td>
+                            <td>
+                                <NumberAndDollar
+                                    value={`${bigNumberToNative(i.amountNative, i.asset)} ${
+                                        width > 600 ? i.asset : ''
+                                    }`}
+                                    dollar={i.amount}
+                                    size="xs"
+                                    color="text-brand-black"
+                                />
+                            </td>
+                            <td>
+                                <BasicToggle
+                                    checked={checked[index]}
+                                    onClick={(e: any) => {
+                                        e.preventDefault();
+                                        openDialog('toggle-collateral-dialog', {
+                                            ...i,
+                                            checked,
+                                            setChecked,
+                                            index,
+                                            trancheId: i.trancheId,
+                                        });
+                                        e.stopPropagation();
+                                    }}
+                                    size="small"
+                                />
+                            </td>
+                            <td>{i.apy}%</td>
+                            {withHealth && (
+                                <td>
+                                    <HealthFactor
+                                        withChange={false}
+                                        trancheId={i.trancheId.toString()}
+                                        showInfo={false}
+                                    />
+                                </td>
+                            )}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
     );
 };
