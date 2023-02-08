0c0bccc747e22e795f5bb0a115b7c5029c443175
yoyobojo <angellolazar@gmail.com>
2023-02-08 11:41:54 -0500

    accordions working - styling needed

--- src/ui/components/toggles/index.ts
+++ src/ui/components/toggles/index.ts
@@ -1,3 +1,4 @@
 export * from './default';
 export * from './checkbox';
 export * from './theme';
+export * from './accordion';
--- src/ui/tables/portfolio/your-positions.tsx
+++ src/ui/tables/portfolio/your-positions.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button } from '../../components';
+import { Card, Button, DefaultAccordion, HealthFactor } from '../../components';
 import {
     YourBorrowsTable,
     YourSuppliesTable,
@@ -12,17 +12,9 @@ interface IYourPositionsProps {
     type: 'borrows' | 'supplies';
     data?: IYourBorrowsTableItemProps[] | IYourSuppliesTableItemProps[];
     isLoading?: boolean;
-    withHealth?: boolean;
-    healthLoading?: boolean;
 }
 
-export const YourPositionsTable: React.FC<IYourPositionsProps> = ({
-    type,
-    data,
-    isLoading,
-    withHealth,
-    healthLoading,
-}) => {
+export const YourPositionsTable: React.FC<IYourPositionsProps> = ({ type, data, isLoading }) => {
     const navigate = useNavigate();
     const determineTitle = () => {
         switch (type) {
@@ -42,31 +34,83 @@ export const YourPositionsTable: React.FC<IYourPositionsProps> = ({
         }
     };
 
+    const formattedExpandables:
+        | IYourSuppliesTableItemProps[]
+        | IYourBorrowsTableItemProps[]
+        | any[] = data
+        ? (data as any).reduce((acc: any, each: any) => {
+              let index = acc.length - 1;
+              if (acc.length && acc[index][0].trancheId == each.trancheId) {
+                  acc[index].push(each);
+              } else {
+                  acc.push([each]);
+              }
+              return acc;
+          }, [])
+        : [];
+
     const determineTable = () => {
         switch (type) {
             case 'supplies':
-                return (
-                    <YourSuppliesTable
-                        withHealth={withHealth}
-                        healthLoading={healthLoading}
-                        data={(data as IYourSuppliesTableItemProps[]) || []}
+                return formattedExpandables.map((el, i) => (
+                    <DefaultAccordion
+                        key={`accordion-supplies-${i}`}
+                        title={`accordion-supplies-${i}`}
+                        summary={
+                            <div className="grid items-center grid-cols-3 w-full">
+                                <span>{el[0].trancheId}</span>
+                                <span className="">{el[0].tranche}</span>
+                                <span className="justify-self-end">
+                                    <HealthFactor
+                                        withChange={false}
+                                        trancheId={el[0].trancheId.toString()}
+                                        showInfo={false}
+                                    />
+                                </span>
+                            </div>
+                        }
+                        details={
+                            <YourSuppliesTable data={(el as IYourSuppliesTableItemProps[]) || []} />
+                        }
                     />
-                );
+                ));
             case 'borrows':
-                return (
-                    <YourBorrowsTable
-                        withHealth={withHealth}
-                        healthLoading={healthLoading}
-                        data={(data as IYourBorrowsTableItemProps[]) || []}
+                return formattedExpandables.map((el, i) => (
+                    <DefaultAccordion
+                        key={`accordion-supplies-${i}`}
+                        title={`accordion-supplies-${i}`}
+                        summary={
+                            <div className="grid items-center grid-cols-3 w-full">
+                                <span>{el[0].trancheId}</span>
+                                <span className="">{el[0].tranche}</span>
+                                <span className="justify-self-end">
+                                    <HealthFactor
+                                        withChange={false}
+                                        trancheId={el[0].trancheId.toString()}
+                                        showInfo={false}
+                                    />
+                                </span>
+                            </div>
+                        }
+                        details={
+                            <YourBorrowsTable data={(el as IYourSuppliesTableItemProps[]) || []} />
+                        }
                     />
-                );
+                ));
         }
     };
 
     return (
         <Card loading={isLoading} title={`Your ${determineTitle()}`} titleClass="text-lg mb-8">
             {data && data.length !== 0 ? (
-                <div>{determineTable()}</div>
+                <div>
+                    <div className="grid items-center grid-cols-3 w-full text-sm font-semibold pr-4 pl-10">
+                        <span className="">ID</span>
+                        <span className="">Tranche</span>
+                        <span className="justify-self-end">Health</span>
+                    </div>
+                    {determineTable()}
+                </div>
             ) : (
                 <div className="w-full flex-col my-10 lg:my-20 text-center">
                     <div className="mb-5">
