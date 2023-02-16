2460996f2927e3f9936adcb6ee5015f43f7c1773
yoyobojo <angellolazar@gmail.com>
2023-02-16 10:56:20 -0500

    fixed estimate gas

--- src/ui/modals/variations/borrow-asset.tsx
+++ src/ui/modals/variations/borrow-asset.tsx
@@ -161,7 +161,7 @@ export const BorrowAssetDialog: React.FC<ISupplyBorrowProps> = ({ name, isOpen, 
                           test: SDK_PARAMS.test,
                           providerRpc: SDK_PARAMS.providerRpc,
                       });
-                setEstimatedGasCost(bigNumberToUSD(res, 18));
+                setEstimatedGasCost(bigNumberToUSD(res, DECIMALS.get(data.asset) || 18));
             }
         };
         getter();
