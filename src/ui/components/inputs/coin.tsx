e92a52ba5c37bf8fbc42dead846b77689d7db76a
yoyobojo <angellolazar@gmail.com>
2023-02-17 16:01:44 -0500

    added usd prices and fixed the create tranche input

--- src/ui/components/inputs/coin.tsx
+++ src/ui/components/inputs/coin.tsx
@@ -1,9 +1,11 @@
 import { AssetDisplay } from '../displays/asset';
 import React from 'react';
-import { NETWORK, SDK_PARAMS } from '../../../utils/sdk-helpers';
+import { bigNumberToUSD, DECIMALS, NETWORK, SDK_PARAMS } from '../../../utils/sdk-helpers';
 import { useSigner } from 'wagmi';
 import { mintTokens } from '@vmexfinance/sdk';
 import { Button } from '../buttons';
+import { usePricesData } from '../../../api/prices';
+import { BigNumber, utils } from 'ethers';
 
 export interface ICoinInput {
     amount: string;
@@ -32,7 +34,7 @@ export const CoinInput = ({
     customMaxClick,
 }: ICoinInput) => {
     const { data: signer } = useSigner();
-
+    const { prices } = usePricesData();
     const onChange = (e: any) => {
         const myamount = e.target.value;
         const isFirstDecimal = amount.length === 0 && myamount === '.';
@@ -56,6 +58,19 @@ export const CoinInput = ({
         }
     };
 
+    const calculateUsd = () => {
+        const decimals = 18;
+        let usdBig;
+        if (prices) {
+            usdBig = (prices as any)[coin.name].usdPrice;
+        } else {
+            usdBig = BigNumber.from('0');
+        }
+        const bigNumAmount = utils.parseUnits(amount || '0', decimals);
+        console.log('USD:', bigNumberToUSD(usdBig.mul(bigNumAmount), decimals));
+        return bigNumberToUSD(usdBig.mul(bigNumAmount), decimals);
+    };
+
     const mint = async () => {
         if (!process.env.REACT_APP_TEST) return;
         if (signer && coin) {
@@ -72,47 +87,51 @@ export const CoinInput = ({
     };
 
     return (
-        <div className="w-full flex flex-row justify-between mt-1 rounded-xl border border-neutral-300 dark:border-neutral-700 p-2">
-            <div className="flex flex-col justify-between gap-3">
-                <input
-                    type="text"
-                    value={amount}
-                    onChange={onChange}
-                    className="text-2xl focus:outline-none max-w-[200px] dark:bg-brand-black overflow-auto dark:placeholder:text-neutral-700"
-                    placeholder="0.00"
-                />
-                {/* <div className="text-neutral400">USD</div> */}
-                {/* TODO: add usd value underneath */}
-                {process.env.REACT_APP_TEST && (
+        <>
+            <div className="w-full flex flex-row justify-between mt-1 rounded-xl border border-neutral-300 dark:border-neutral-700 p-2">
+                <div className="flex flex-col justify-between gap-3">
+                    <input
+                        type="text"
+                        value={amount}
+                        onChange={onChange}
+                        className="text-2xl focus:outline-none max-w-[200px] dark:bg-brand-black overflow-auto dark:placeholder:text-neutral-700"
+                        placeholder="0.00"
+                    />
+                    {/* TODO: add usd value */}
+                    {/* <div className="text-neutral-400">{calculateUsd()} USD</div> */}
+                </div>
+                <div className="flex flex-col justify-between items-end gap-3">
+                    <AssetDisplay logo={coin.logo} name={coin.name} />
+                    <button
+                        className={`text-xs text-right text-blue-700 dark:text-brand-blue dark:hover:text-blue-500 hover:text-brand-purple transition duration-150 ${
+                            loading ? 'animate-pulse' : ''
+                        }`}
+                        onClick={onMaxButtonClick}
+                    >
+                        <span>MAX</span>
+                        <p>
+                            {`${
+                                type === 'collateral'
+                                    ? 'Amount Borrowable'
+                                    : type === 'owed'
+                                    ? 'Amount Owed'
+                                    : 'Balance'
+                            }:`}{' '}
+                            {balance || 0}
+                        </p>
+                    </button>
+                </div>
+            </div>
+            {process.env.REACT_APP_TEST && (
+                <div className="mt-2 flex justify-end">
                     <Button
                         primary
                         onClick={mint}
                         label={`DEV: Mint ${coin.name}`}
                         className="w-fit"
                     />
-                )}
-            </div>
-            <div className="flex flex-col justify-between items-end gap-3">
-                <AssetDisplay logo={coin.logo} name={coin.name} />
-                <button
-                    className={`text-xs text-right text-blue-700 dark:text-brand-blue dark:hover:text-blue-500 hover:text-brand-purple transition duration-150 ${
-                        loading ? 'animate-pulse' : ''
-                    }`}
-                    onClick={onMaxButtonClick}
-                >
-                    <span>MAX</span>
-                    <p>
-                        {`${
-                            type === 'collateral'
-                                ? 'Amount Borrowable'
-                                : type === 'owed'
-                                ? 'Amount Owed'
-                                : 'Balance'
-                        }:`}{' '}
-                        {balance || 0}
-                    </p>
-                </button>
-            </div>
-        </div>
+                </div>
+            )}
+        </>
     );
 };
