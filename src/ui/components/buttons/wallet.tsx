1ba3421160e86c30ed78c6fb91d1d92f08794c22
yoyobojo <angellolazar@gmail.com>
2023-07-16 11:07:35 -0400

    added no data and loading display to claim modal

--- src/ui/components/buttons/wallet.tsx
+++ src/ui/components/buttons/wallet.tsx
@@ -137,7 +137,7 @@ export const WalletButton = ({ primary, className, label = 'Connect Wallet' }: I
                                             'min-h-[36px] !py-2',
                                             mode,
                                             className,
-                                            'bg-brand-black dark:bg-neutral-200 dark:text-neutral-900 dark:hover:bg-neutral-300 rounded-lg text-neutral-100 hover:bg-neutral-800 border border-brand-black',
+                                            'bg-brand-black dark:bg-neutral-200 dark:text-neutral-900 dark:hover:bg-neutral-300 rounded-lg text-neutral-900 hover:text-neutral-100 hover:bg-neutral-800 border border-brand-black',
                                         ].join(' ')}
                                     >
                                         {determineConnection().render}
