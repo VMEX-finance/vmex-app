93c7c45cd84a0f98a672d0932492e798615f882a
yoyobojo <angellolazar@gmail.com>
2023-07-16 11:07:35 -0400

    added no data and loading display to claim modal

--- src/ui/modals/subcomponents/table-display.tsx
+++ src/ui/modals/subcomponents/table-display.tsx
@@ -1,3 +1,4 @@
+import { SpinnerLoader } from '../../components/loaders';
 import React, { ReactNode } from 'react';
 
 type IModalTableItemProps = {
@@ -10,39 +11,70 @@ type IModalTableItemProps = {
 type IModalTableDisplayProps = {
     title?: string;
     content: IModalTableItemProps[];
+    noData?: {
+        text: string;
+        ctaText?: string;
+        ctaLink?: string;
+    };
+    loading?: boolean;
 };
 
-export const ModalTableDisplay = ({ title, content }: IModalTableDisplayProps) => {
+export const ModalTableDisplay = ({ title, content, noData, loading }: IModalTableDisplayProps) => {
     return (
         <>
-            {title && <h3 className="mt-6 text-neutral400">{title}</h3>}
+            {title && <h3 className="mt-6">{title}</h3>}
             <div
-                className={`mt-2 flex justify-between rounded-lg border border-neutral-300 dark:border-neutral-700 p-4 lg:py-6`}
+                className={`${
+                    content?.length ? 'justify-between' : 'justify-center'
+                } mt-2 flex rounded-lg border border-neutral-300 dark:border-neutral-700 p-4 lg:py-6`}
             >
-                <div className="flex flex-col gap-2">
-                    {content.map((el, i) => (
-                        <span key={i}>{el.label}</span>
-                    ))}
-                </div>
+                {loading ? (
+                    <>
+                        <SpinnerLoader height="auto" size="sm" />
+                    </>
+                ) : (
+                    <>
+                        {content?.length ? (
+                            <>
+                                <div className="flex flex-col gap-2">
+                                    {content.map((el, i) => (
+                                        <span key={i}>{el.label}</span>
+                                    ))}
+                                </div>
 
-                <div className="min-w-[100px] flex flex-col gap-2">
-                    {content.map((el, i) => (
-                        <span
-                            key={i}
-                            className={`${el.loading ? 'animate-pulse' : ''} ${
-                                el.baseLink
-                                    ? 'underline text-brand-purple hover:text-opacity-80 duration-200 transition cursor-pointer'
-                                    : ''
-                            }`}
-                        >
-                            {el.baseLink ? (
-                                <a href={`${el.baseLink}/${el.value}`}>{el.value}</a>
-                            ) : (
-                                el.value
-                            )}
-                        </span>
-                    ))}
-                </div>
+                                <div className="min-w-[100px] flex flex-col gap-2">
+                                    {content.map((el, i) => (
+                                        <span
+                                            key={i}
+                                            className={`${el.loading ? 'animate-pulse' : ''} ${
+                                                el.baseLink
+                                                    ? 'underline text-brand-purple hover:text-opacity-80 duration-200 transition cursor-pointer'
+                                                    : ''
+                                            }`}
+                                        >
+                                            {el.baseLink ? (
+                                                <a href={`${el.baseLink}/${el.value}`}>
+                                                    {el.value}
+                                                </a>
+                                            ) : (
+                                                el.value
+                                            )}
+                                        </span>
+                                    ))}
+                                </div>
+                            </>
+                        ) : (
+                            <div className="flex flex-col justify-center items-center">
+                                <span className="text-center">
+                                    {noData?.text || 'No Data Available'}
+                                </span>
+                                {noData?.ctaLink && noData?.ctaText && (
+                                    <a href={`/#${noData.ctaLink}`}>{noData.ctaText}</a>
+                                )}
+                            </div>
+                        )}
+                    </>
+                )}
             </div>
         </>
     );
