74fd0488ba22988bf256d273dd1472c50edeb554
alo <0xyoyobojo@gmail.com>
2023-08-13 16:26:30 -0400

    rewards coming in

--- src/ui/tables/portfolio/your-rewards.tsx
+++ src/ui/tables/portfolio/your-rewards.tsx
@@ -23,59 +23,65 @@ export const YourRewardsTable: React.FC<IYourRewardsTableProps> = ({ data, isLoa
 
     return (
         <Card loading={isLoading} title={`Your Rewards`} titleClass="text-lg mb-2">
-            <table className="min-w-full divide-y-2 divide-gray-300 dark:divide-neutral-800 font-basefont">
-                <thead className="">
-                    <tr>
-                        {headers.map((el, i) => (
-                            <th
-                                key={`table-header-${i}`}
-                                scope="col"
-                                className={`py-3 text-left text-sm font-semibold text-neutral900 first-of-type:pl-2 first-of-type:md:pl-6`}
-                            >
-                                {el}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 dark:divide-neutral-800">
-                    {data &&
-                        data.map((i) => {
-                            return (
-                                <tr
-                                    key={`reward-table-row-${i.asset}`}
-                                    className="text-left transition duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-900 hover:cursor-pointer"
-                                    onClick={
-                                        () => {}
-                                        // openDialog('borrow-asset-dialog', {
-                                        //     ...i,
-                                        //     view: 'Repay',
-                                        // })
-                                    }
+            {data.length ? (
+                <table className="min-w-full divide-y-2 divide-gray-300 dark:divide-neutral-800 font-basefont">
+                    <thead className="">
+                        <tr>
+                            {headers.map((el, i) => (
+                                <th
+                                    key={`table-header-${i}`}
+                                    scope="col"
+                                    className={`py-3 text-left text-sm font-semibold text-neutral900 first-of-type:pl-2 first-of-type:md:pl-6`}
                                 >
-                                    <td className="whitespace-nowrap p-4 text-sm sm:pl-6">
-                                        <AssetDisplay
-                                            name={width > 600 ? i.asset : ''}
-                                            logo={`/coins/${i.asset?.toLowerCase()}.svg`}
-                                        />
-                                    </td>
-                                    <td>
-                                        <NumberAndDollar
-                                            value={`${i.amountNative} ${
-                                                width > 600 ? i.asset : ''
-                                            }`}
-                                            dollar={i.amountUsd}
-                                            size="xs"
-                                            color="text-brand-black"
-                                        />
-                                    </td>
-                                    <td>
-                                        <Button label="Claim" primary />
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                </tbody>
-            </table>
+                                    {el}
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 dark:divide-neutral-800">
+                        {data &&
+                            data.map((i) => {
+                                return (
+                                    <tr
+                                        key={`reward-table-row-${i.asset}`}
+                                        className="text-left transition duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-900 hover:cursor-pointer"
+                                        onClick={
+                                            () => {}
+                                            // openDialog('borrow-asset-dialog', {
+                                            //     ...i,
+                                            //     view: 'Repay',
+                                            // })
+                                        }
+                                    >
+                                        <td className="whitespace-nowrap p-4 text-sm sm:pl-6">
+                                            <AssetDisplay
+                                                name={width > 600 ? i.asset : ''}
+                                                logo={`/coins/${i.asset?.toLowerCase()}.svg`}
+                                            />
+                                        </td>
+                                        <td>
+                                            <NumberAndDollar
+                                                value={`${i.amountNative} ${
+                                                    width > 600 ? i.asset : ''
+                                                }`}
+                                                dollar={i.amountUsd}
+                                                size="xs"
+                                                color="text-brand-black"
+                                            />
+                                        </td>
+                                        <td>
+                                            <Button label="Claim" primary />
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                    </tbody>
+                </table>
+            ) : (
+                <span className="flex justify-center text-center py-12 lg:py-16">
+                    No Rewards Available
+                </span>
+            )}
         </Card>
     );
 };
