22c940f74f9fea14314b6f200819946fb396768f
Filip Lasic <filip.lasic89@gmail.com>
2023-08-24 17:53:37 +0200

    claim external rewards
    updated dependencies

--- src/ui/tables/portfolio/your-rewards.tsx
+++ src/ui/tables/portfolio/your-rewards.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import { AssetDisplay, NumberAndDollar } from '../../components/displays';
-import { BigNumber } from 'ethers';
-import { bigNumberToNative } from '../../../utils';
-import { useWindowSize, useDialogController } from '../../../hooks';
+import { useWindowSize } from '../../../hooks';
 import { Button, Card } from '../../components';
+import { useNetwork, useSigner } from 'wagmi';
+import { SDK_PARAMS } from '../../../utils/constants';
+import { claimExternalRewards } from '@vmexfinance/sdk';
 
 export type IYourRewardsTableItemProps = {
     asset: string;
     amountUsd: string;
-    amountNative: BigNumber;
+    amountNative: string;
+    token: string;
+    proof: string[];
+    amountWei: string;
 };
 
 export type IYourRewardsTableProps = {
     data: IYourRewardsTableItemProps[];
     isLoading: boolean;
+    address: string;
 };
 
-export const YourRewardsTable: React.FC<IYourRewardsTableProps> = ({ data, isLoading }) => {
+export const YourRewardsTable: React.FC<IYourRewardsTableProps> = ({
+    data,
+    isLoading,
+    address,
+}) => {
     const { width } = useWindowSize();
-    const { openDialog } = useDialogController();
     const headers = ['Asset', 'Amount', ''];
+    const { data: signer } = useSigner();
+    const { chain } = useNetwork();
+
+    const onClaim = async (
+        account: string,
+        rewardToken: string,
+        amountWei: string,
+        proof: string[],
+    ) => {
+        if (!signer || !chain) return;
+        console.log(account, rewardToken, amountWei, proof);
+        const tx = await claimExternalRewards(
+            signer,
+            chain?.network,
+            account,
+            rewardToken,
+            amountWei,
+            proof,
+            false,
+            SDK_PARAMS.providerRpc,
+        );
+        await tx.wait();
+    };
 
     return (
         <Card loading={isLoading} title={`Your Rewards`} titleClass="text-lg mb-2">
@@ -40,10 +71,10 @@ export const YourRewardsTable: React.FC<IYourRewardsTableProps> = ({ data, isLoa
                     </thead>
                     <tbody className="divide-y divide-gray-200 dark:divide-neutral-800">
                         {data &&
-                            data.map((i) => {
+                            data.map((reward) => {
                                 return (
                                     <tr
-                                        key={`reward-table-row-${i.asset}`}
+                                        key={`reward-table-row-${reward.token}`}
                                         className="text-left transition duration-200 hover:bg-neutral-200 dark:hover:bg-neutral-900 hover:cursor-pointer"
                                         onClick={
                                             () => {}
@@ -55,22 +86,31 @@ export const YourRewardsTable: React.FC<IYourRewardsTableProps> = ({ data, isLoa
                                     >
                                         <td className="whitespace-nowrap p-4 text-sm sm:pl-6">
                                             <AssetDisplay
-                                                name={width > 600 ? i.asset : ''}
-                                                logo={`/coins/${i.asset?.toLowerCase()}.svg`}
+                                                name={width > 600 ? reward.asset : ''}
+                                                logo={`/coins/${reward.token}.svg`}
                                             />
                                         </td>
                                         <td>
                                             <NumberAndDollar
-                                                value={`${i.amountNative} ${
-                                                    width > 600 ? i.asset : ''
-                                                }`}
-                                                dollar={i.amountUsd}
+                                                value={reward.amountNative}
+                                                dollar={reward.amountUsd}
                                                 size="xs"
                                                 color="text-brand-black"
                                             />
                                         </td>
                                         <td>
-                                            <Button label="Claim" primary />
+                                            <Button
+                                                label="Claim"
+                                                primary
+                                                onClick={() =>
+                                                    onClaim(
+                                                        address,
+                                                        reward.token,
+                                                        reward.amountWei,
+                                                        reward.proof,
+                                                    )
+                                                }
+                                            />
                                         </td>
                                     </tr>
                                 );
