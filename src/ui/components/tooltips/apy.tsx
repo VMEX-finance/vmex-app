b4f25af8878a7fc27924804de2374d8e849e9aab
alo <0xyoyobojo@gmail.com>
2023-09-18 17:51:40 -0400

    formatted tooltips a bit

--- src/ui/components/tooltips/apy.tsx
+++ src/ui/components/tooltips/apy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useApyData } from '@/api';
-import { findInObjArr, percentFormatter } from '@/utils';
+import { capFirstLetter, findInObjArr, getRandomNumber, percentFormatter } from '@/utils';
 import { Tooltip } from './default';
 
 export const ApyToolitp = ({ symbol, oldApy }: { symbol?: string; oldApy?: number | string }) => {
@@ -13,19 +13,32 @@ export const ApyToolitp = ({ symbol, oldApy }: { symbol?: string; oldApy?: numbe
         const { apysByToken, asset, assetType, name, symbol, totalApy } = rewardApy;
         const percent = percentFormatter.format(Number(totalApy) / 100);
         if (apysByToken?.length) {
+            console.log('suppy apy', apysByToken);
             return (
-                <Tooltip id={asset} content={percent}>
+                <Tooltip id={`${asset}-${getRandomNumber()}`} content={percent}>
                     <div className="min-w-[120px]">
                         <span className="font-bold">APY Breakdown</span>
-                        {apysByToken.map((x: any) => (
-                            <div
-                                className="text-sm flex justify-between items-center gap-6"
-                                key={`reward-apy-tooltip-${x.asset}`}
-                            >
-                                <span>{x?.symbol || x.asset}</span>
-                                <span>{percentFormatter.format(Number(x.apy) / 100)}</span>
-                            </div>
-                        ))}
+                        <ul>
+                            {apysByToken
+                                .sort((a: any, b: any) => a.symbol.length - b.symbol.length)
+                                .map((x: any) => (
+                                    <li
+                                        className={`text-sm flex justify-between items-center gap-6 ${
+                                            x?.symbol === 'yield' ? 'border-t mt-1 pt-1' : ''
+                                        }`}
+                                        key={`reward-apy-tooltip-${asset}-${
+                                            x.asset
+                                        }-${getRandomNumber()}`}
+                                    >
+                                        {x?.symbol?.length >= 5 ? (
+                                            <span>{capFirstLetter(x?.symbol) || x.asset}</span>
+                                        ) : (
+                                            <span>{x?.symbol || x.asset}</span>
+                                        )}
+                                        <span>{percentFormatter.format(Number(x.apy) / 100)}</span>
+                                    </li>
+                                ))}
+                        </ul>
                     </div>
                 </Tooltip>
             );
