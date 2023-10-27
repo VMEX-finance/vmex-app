4717381eadd75f2969c0140759172c781d22528c
Filip Lasic <filip.lasic89@gmail.com>
2023-10-27 17:04:54 +0200

    getAllAssetApys check lowercase addresses

--- src/api/rewards/apy.tsx
+++ src/api/rewards/apy.tsx
@@ -5,7 +5,7 @@ import { getNetwork } from '@wagmi/core';
 import { convertAddressToSymbol } from '@vmexfinance/sdk';
 import { ethers } from 'ethers';
 
-export async function getAllAssetApys() {
+export async function getAllAssetApys(): Promise<IAssetApyProps[]> {
     const network = getNetwork()?.chain?.unsupported
         ? DEFAULT_NETWORK
         : getNetwork()?.chain?.network || DEFAULT_NETWORK;
@@ -16,47 +16,46 @@ export async function getAllAssetApys() {
 
     const { apy, tokenDetails }: { apy: any[]; tokenDetails: any[] } = await res.json();
 
-    const formattedApy: IAssetApyProps[] = apy?.length
-        ? await Promise.all(
-              apy.map(async (a) => {
-                  const found = findInObjArr('address', a.asset, tokenDetails);
-                  const apysByToken: any[] = [];
-                  if (a?.apysByToken) {
-                      a.apysByToken.forEach(async (t: any) => {
-                          if (t.name) {
-                              apysByToken.push({
-                                  apy: t.apy,
-                                  symbol: t.name,
-                                  name: t.name,
-                              });
-                          } else {
-                              const foundVaultToken = findInObjArr(
-                                  'address',
-                                  t.token,
-                                  tokenDetails,
-                              );
-                              apysByToken.push({
-                                  asset: t.token,
-                                  apy: t.apy,
-                                  symbol: foundVaultToken
-                                      ? foundVaultToken.symbol
-                                      : convertAddressToSymbol(t.token, network),
-                                  name: foundVaultToken?.name || '',
-                              });
-                          }
-                      });
-                  }
-                  return {
-                      ...a,
-                      symbol: found ? found.symbol : convertAddressToSymbol(a.asset, network),
-                      name: found?.name || '',
-                      apysByToken,
-                      description: found?.description || '',
-                  };
-              }),
-          )
-        : [];
-    return formattedApy;
+    if (!apy?.length) return [];
+
+    return await Promise.all(
+        apy.map(async (a) => {
+            const found = tokenDetails.find(
+                (x) => x.address.toLowerCase() === a.asset.toLowerCase(),
+            );
+            const apysByToken: any[] = [];
+            if (a?.apysByToken) {
+                a.apysByToken.forEach(async (t: any) => {
+                    if (t.name) {
+                        apysByToken.push({
+                            apy: t.apy,
+                            symbol: t.name,
+                            name: t.name,
+                        });
+                    } else {
+                        const foundVaultToken = tokenDetails.find(
+                            (x) => x.address.toLowerCase() === t.token.toLowerCase(),
+                        );
+                        apysByToken.push({
+                            asset: t.token,
+                            apy: t.apy,
+                            symbol: foundVaultToken
+                                ? foundVaultToken.symbol
+                                : convertAddressToSymbol(t.token, network),
+                            name: foundVaultToken?.name || '',
+                        });
+                    }
+                });
+            }
+            return {
+                ...a,
+                symbol: found ? found.symbol : convertAddressToSymbol(a.asset, network),
+                name: found?.name || '',
+                apysByToken,
+                description: found?.description || '',
+            };
+        }),
+    );
 }
 
 export function useApyData() {
