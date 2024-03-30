f62987da32659dcc9ad9cecb8f6e41e4fa73484e
ksyao <ksyao2002@gmail.com>
2024-03-30 16:34:13 -0500

    direct main fix get request

--- src/api/asset-apy.tsx
+++ src/api/asset-apy.tsx
@@ -7,51 +7,55 @@ import { useEffect, useMemo } from 'react';
 
 export async function getAllAssetApys(): Promise<IAssetApyProps[]> {
     const network = getNetworkName();
+    try {
+        const res = await fetch(`${NETWORKS[network].backend}/v1/reward/apy`);
+        if (res.status !== 200) return [];
 
-    const res = await fetch(`${NETWORKS[network].backend}/v1/reward/apy`);
-    if (res.status !== 200) return [];
+        const { apy, tokenDetails }: { apy: any[]; tokenDetails: any[] } = await res.json();
+        if (!apy?.length) return [];
 
-    const { apy, tokenDetails }: { apy: any[]; tokenDetails: any[] } = await res.json();
-    if (!apy?.length) return [];
-
-    return await Promise.all(
-        apy.map(async (a) => {
-            const found = tokenDetails.find(
-                (x) => x.address.toLowerCase() === a.asset.toLowerCase(),
-            );
-            const apysByToken: any[] = [];
-            if (a?.apysByToken) {
-                a.apysByToken.forEach(async (t: any) => {
-                    if (t.name) {
-                        apysByToken.push({
-                            apy: t.apy,
-                            symbol: t.name,
-                            name: t.name,
-                        });
-                    } else {
-                        const foundVaultToken = tokenDetails.find(
-                            (x) => x.address.toLowerCase() === t.token.toLowerCase(),
-                        );
-                        apysByToken.push({
-                            asset: t.token,
-                            apy: t.apy,
-                            symbol: foundVaultToken
-                                ? foundVaultToken.symbol
-                                : convertAddressToSymbol(t.token, network),
-                            name: foundVaultToken?.name || '',
-                        });
-                    }
-                });
-            }
-            return {
-                ...a,
-                symbol: found ? found.symbol : convertAddressToSymbol(a.asset, network),
-                name: found?.name || '',
-                apysByToken,
-                description: found?.description || '',
-            };
-        }),
-    );
+        return await Promise.all(
+            apy.map(async (a) => {
+                const found = tokenDetails.find(
+                    (x) => x.address.toLowerCase() === a.asset.toLowerCase(),
+                );
+                const apysByToken: any[] = [];
+                if (a?.apysByToken) {
+                    a.apysByToken.forEach(async (t: any) => {
+                        if (t.name) {
+                            apysByToken.push({
+                                apy: t.apy,
+                                symbol: t.name,
+                                name: t.name,
+                            });
+                        } else {
+                            const foundVaultToken = tokenDetails.find(
+                                (x) => x.address.toLowerCase() === t.token.toLowerCase(),
+                            );
+                            apysByToken.push({
+                                asset: t.token,
+                                apy: t.apy,
+                                symbol: foundVaultToken
+                                    ? foundVaultToken.symbol
+                                    : convertAddressToSymbol(t.token, network),
+                                name: foundVaultToken?.name || '',
+                            });
+                        }
+                    });
+                }
+                return {
+                    ...a,
+                    symbol: found ? found.symbol : convertAddressToSymbol(a.asset, network),
+                    name: found?.name || '',
+                    apysByToken,
+                    description: found?.description || '',
+                };
+            }),
+        );
+    } catch (error) {
+        console.error('Error fetching APY:', error);
+        return [];
+    }
 }
 
 export function useApyData() {
